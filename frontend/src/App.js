635def076e3cfc0e44984c39a0ab8248286badf7
Julian Frattini <julian.frattini@alice-dsl.net>
2022-03-23 12:00:58 +0100

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,56 +3,28 @@ import './App.scss'
 
 function App() {
   // create a react state for the list of tasks to display
-  const [data, setData] = useState([{
-    id: 0,
-    title: 'Learn react',
-    description: 'Learn the fundamentals of the react JS frontend framework',
-    done: false
-  }])
+  const [tasks, setTasks] = useState([])
+  
 
-  return (
-    <div>
-      <h1>Tasks</h1>
-      <div className='container'>
-        <div className='element'>
-          <a href='https://www.youtube.com/watch?v=QdHvS0D1zAI' target='_blank'>
-            <img src='http://i3.ytimg.com/vi/QdHvS0D1zAI/hqdefault.jpg'></img>
-          </a>
-          <div className="title-overlay">Hosting a Website on Raspberry Pi</div>
-        </div>
-        <div className='element'>
-          <a href='https://www.youtube.com/watch?v=Sxxw3qtb3_g' target='_blank'>
-            <img src='http://i3.ytimg.com/vi/Sxxw3qtb3_g/hqdefault.jpg'></img>
-          </a>
-          <div className="title-overlay">Tech Stacks</div>
-        </div>
-        <div className='element'>
-          <a href='https://www.youtube.com/watch?v=Qhaz36TZG5Y' target='_blank'>
-            <img src='http://i3.ytimg.com/vi/Qhaz36TZG5Y/hqdefault.jpg'></img>
-          </a>
-          <div className="title-overlay">CSS Tips</div>
-        </div>
-      </div>
-    </div>
-  );
-
-  /*useEffect(() => {
-    fetch('http://localhost:5000/tasks').then(
+  useEffect(() => {
+    fetch('http://localhost:5000/tasks/John/Doe').then(
       // cast the received data to json
       res => res.json()
     ).then(
-      data => {
+      user => {
         // convert the tasks into the required format
-        const convertedTasks = data.map(item => {
+        console.log(user)
+
+        const convertedTasks = user['tasks'].map(item => {
           return {
             id: item._id['$oid'],
             title: item.title,
             description: item.description,
-            done: item.done
+            url: item.video.url
           }
         });
         // store the received tasks in the state
-        setData(convertedTasks);
+        setTasks(convertedTasks);
       }
     ).catch(function(error) {
       console.log(error)
@@ -61,14 +33,20 @@ function App() {
 
   return (
     <div>
-      {data.length === 0 ? 
-        <p>not there</p> : 
-        <ul>
-          {data.map((task, i) => <li key={task.id}>{task.title}: {task.description} {task.done ? 'done' : 'not done'}</li>)}
-        </ul>
-      }
+      <h1>Tasks</h1>
+      <div className='container'>
+        {
+          tasks.map(task => 
+            <div className='element'>
+              <a href={`https://www.youtube.com/watch?v=${task.url}`} target='_blank'>
+                <img src={`http://i3.ytimg.com/vi/${task.url}/hqdefault.jpg`}></img>
+              </a>
+              <div className="title-overlay">{task.title}</div>
+            </div>)
+        }
+      </div>
     </div>
-  )*/
+  );
 }
 
 export default App
