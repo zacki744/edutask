f4ae8bd513bd311952ebc9c801d9ab32cfb26f3d
Julian Frattini <julian.frattini@alice-dsl.net>
2022-03-30 16:52:22 +0200

    Connected tasklist to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,34 +5,12 @@ import NavBar from './Components/NavBar'
 
 function App() {
   // create a react state for the list of tasks to display
-  const [tasks, setTasks] = useState([])
   const [email, setEmail] = useState('')
 
   const [user, setUser] = useState({
     "_id": "624442cfa6ba7231cec8f2c2"
   })
   
-  useEffect(() => {
-    fetch(`http://localhost:5000/tasks/ofuser/${user['_id']}`)
-      .then(res => res.json())
-      .then(
-        tasks => {
-          // convert the tasks into the required format
-          const convertedTasks = tasks.map(item => {
-            return {
-              id: item._id['$oid'],
-              title: item.title,
-              description: item.description,
-              url: item.video.url
-            }
-          });
-          // store the received tasks in the state
-          setTasks(convertedTasks);
-        }
-      ).catch(function(error) {
-        console.log(error)
-      });
-  }, [user]);
 
   const login = (event) => {
     event.preventDefault();
@@ -65,8 +43,9 @@ function App() {
         </div>
         : <div>
           <h1>Tasks</h1>
+
           <TaskView
-            tasks={tasks}
+            userid={user['_id']}
           />
         </div>}
     </div>
--- frontend/src/Components/TaskView.js
+++ frontend/src/Components/TaskView.js
@@ -1,40 +1,105 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './../Styles/TaskView.css'
 
-const TaskView = props => {
-    const submitNewTask = (event) => {
-        console.log(event)
-    }
-
+function TaskView(props) {
+  function TaskItem(props) {
     return (
-        <div className='container'>
-        {
-          props.tasks.map(task => 
-            <div className='element' key={task.id}>
-              <a href={`https://www.youtube.com/watch?v=${task.url}`} target='_blank' rel="noreferrer">
-                <img src={`http://i3.ytimg.com/vi/${task.url}/hqdefault.jpg`} alt=''></img>
-              </a>
-              <div className="title-overlay">{task.title}</div>
-            </div>)
-        }
-
-        <div className='element' key='newtask'>
-          <button>Create new Task</button>
-          <form onSubmit={submitNewTask}>
-            <div className='inputwrapper'>
-              <label>Title </label>
-              <input type='text' id='title' name='title'></input>
-            </div>
-            <div className='inputwrapper'>
-              <label>Description </label>
-              <input type='text' id='description' name='description'></input>
-            </div>
-
-            <button>Save</button>
-          </form>
-        </div>
+      <div className='element' key={props.id}>
+        { props.children }
       </div>
     );
+  }
+  
+  function TaskCreator(props) {
+    let userid = props.userid
+  
+    const [title, setTitle] = useState("")
+    const [url, setUrl] = useState("")
+  
+    const submitNewTask = (event) => {
+      event.preventDefault()
+  
+      const data = new URLSearchParams();
+      data.append('title', title)
+      data.append('description', "")
+      data.append('userid', userid)
+      data.append('url', url)
+      data.append('todos', ['Watch video'])
+  
+      fetch('http://localhost:5000/tasks/create', {
+        method: 'post', 
+        body: data
+      }).then(res => res.json())
+        .then(console.log('updating'))
+        .then(updateTasks());
+    }
+  
+    return (
+      <form onSubmit={submitNewTask}>
+        <div className='inputwrapper'>
+          <label>Title</label>
+          <input type='text' id='title' name='title' onChange={event => setTitle(event.target.value)}></input>
+        </div>
+        <div className='inputwrapper'>
+          <label>YouTube URL</label>
+          <input type='text' id='url' name='url' onChange={event => setUrl(event.target.value)}></input>
+        </div>
+  
+        <button disabled={title.length == 0}>Create new Task</button>
+      </form>
+    )
+  }
+
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    updateTasks();
+  }, []); 
+
+  const updateTasks = () => {
+    fetch(`http://localhost:5000/tasks/ofuser/${props.userid}`, {
+      method: 'get',
+      headers: {'Cache-Control': 'no-cache'}
+    })
+      .then(res => res.json())
+      .then(
+        res => {
+          // convert the tasks into the required format
+          const convertedTasks = res.map(item => {
+            return {
+              id: item._id['$oid'],
+              title: item.title,
+              description: item.description,
+              url: item.video.url
+            }
+          });
+          // store the received tasks in the state
+          setTasks(convertedTasks);
+          console.log(tasks);
+        }
+      ).catch(function(error) {
+        console.log(error)
+      });
+  }
+
+  return (
+      <div className='container'>
+      {
+        tasks.map(task => 
+          <TaskItem id={task.id}>
+            <a href={`https://www.youtube.com/watch?v=${task.url}`} target='_blank' rel="noreferrer">
+              <img src={`http://i3.ytimg.com/vi/${task.url}/hqdefault.jpg`} alt=''></img>
+              
+              <div className="title-overlay">{task.title}</div>
+            </a>
+          </TaskItem>)
+      }
+
+      <TaskItem id='newtask'>
+        <TaskCreator userid={props.userid}/>
+      </TaskItem>
+    </div>
+  );
 }
 
 export default TaskView
