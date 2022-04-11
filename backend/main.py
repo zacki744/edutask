2d29b0ee43c8dc7e02eb86c50de9183de61caae4
Julian Frattini <julian.frattini@alice-dsl.net>
2022-04-11 11:43:55 +0200

    Refactored dummy database population

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 # coding=utf-8
-import os
+import os, json
 from dotenv import dotenv_values
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+
+from src.blueprints.userblueprint import user_blueprint
+from src.blueprints.taskblueprint import task_blueprint
+from src.blueprints.todoblueprint import todo_blueprint
+
+from src.controllers.usercontroller import UserController
+from src.controllers.taskcontroller import TaskController
+from src.util.daos import getDao
+
 
 app = Flask('todoapp')
 
@@ -12,15 +21,10 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # register blueprints
-from src.blueprints.userblueprint import user_blueprint
-from src.blueprints.taskblueprint import task_blueprint
-from src.blueprints.todoblueprint import todo_blueprint
 app.register_blueprint(blueprint=user_blueprint, url_prefix='/users')
 app.register_blueprint(blueprint=task_blueprint, url_prefix='/tasks')
 app.register_blueprint(blueprint=todo_blueprint, url_prefix='/todos')
 
-import src.controllers.usercontroller as usercont
-import src.controllers.taskcontroller as taskcont
 
 # simple heartbeat method to check if the server is running
 @app.route('/')
@@ -33,30 +37,32 @@ def ping():
 @app.route('/populate', methods=['POST'])
 @cross_origin()
 def populate():
-    janedoe = usercont.create_user({'firstName': 'Jane', 'lastName': 'Doe', 'email': 'jane.doe@gmail.com'})
-    taskcont.create_task({
-        'userid': janedoe['_id']['$oid'],
-        'title': 'Improve Devtools',
-        'description': 'Upgrade the tools used for web development',
-        'url': 'U_gANjtv28g',
-        'todos': ['Watch video', 'Evaluate usability of tools', 'Install viable tools']
-    })
-    taskcont.create_task({
-        'userid': janedoe['_id']['$oid'],
-        'title': 'Tech Stacks',
-        'description': 'Understand the structure of a tech stack and explore popular stacks in use.',
-        'url': 'Sxxw3qtb3_g',
-        'todos': ['Watch video', 'Implement the techstack presented in the video']
-    })
-    taskcont.create_task({
-        'userid': janedoe['_id']['$oid'],
-        'title': 'Javascript Pro Tips',
-        'description': 'Get better at writing JavaScript Code',
-        'url': 'Mus_vwhTCq0',
-        'todos': ['Watch video']
-    })
+    usercontroller = UserController(getDao(collection_name='user'))
+    taskcontroller = TaskController(tasks_dao=getDao(collection_name='task'), videos_dao=getDao(collection_name='video'), todos_dao=getDao(collection_name='todo'), users_dao=getDao(collection_name='user'))
 
-    return jsonify({'response': 'ok'}), 200
+    response = {'users': []}
+    with open(f'./src/static/data/dummy.json', 'r') as f:
+        dummydata = json.load(f)
+
+        for userdata in dummydata:
+            user = usercontroller.create({
+                'firstName': userdata['firstName'], 
+                'lastName': userdata['lastName'], 
+                'email': userdata['email']
+            })
+
+            for taskdata in userdata['tasks']:
+                taskcontroller.create({
+                    'userid': user['_id']['$oid'],
+                    'title': taskdata['title'],
+                    'description': taskdata['description'],
+                    'url': taskdata['url'],
+                    'todos': taskdata['todos']
+                })
+
+            response['users'].append(user['_id']['$oid'])
+
+    return jsonify(response), 200
 
 # main loop
 if __name__ == '__main__':
