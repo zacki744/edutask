b18d9990d5007e67a5dea7851265e9bf455ecba9
zacki744 <78082188+zacki744@users.noreply.github.com>
2024-04-21 22:34:23 +0200

    Update test_usercontroller.py

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,49 +1,75 @@
 import pytest
 from unittest.mock import MagicMock
-
 from src.controllers.usercontroller import UserController
-
 
 class TestUserController:
 
+    # Fixture for creating a UserController instance with a mocked DAO
     @pytest.fixture
-    def sut():
-        mockDAO = MagicMock()
-        mock = UserController(dao=mockDAO)
-        return mock
-    
+    def sut_one_user(self):
+        returnVal = {
+            'firstName': 'John', 
+            'lastName': 'Doe',
+            'email': 'john.doe@example.com',
+        }
+        dao = MagicMock()
+        dao.find.return_value = [returnVal]
+        return UserController(dao)
+
+    #Fixture for multiple users
     @pytest.fixture
-    def email_data():
-    invalid_emails = [
-        ('@email.com',),
-        ('jane.doeemail.com',),
-        ('jane.doe@.com',),
-        ('jane.doe@emailcom',),
-        ('jane.doe@email.',),
-        ('jane@doe@email.com',),
-        ('',),
-    ]
-        vallid = [
-            'john@hotmail.com',
-            'vallid@gmail.com',
+    def sut_multiple_users(self):
+        returnVal = [
+            {
+                'firstName': 'John', 
+                'lastName': 'Doe',
+                'email': 'john123@example.com'
+            },
+            {
+                'firstName': 'John', 
+                'lastName': 'Doe',
+                'email': 'john123@example.com'
+            }
         ]
-        return vallid, invalid_emails
-    
+        dao = MagicMock()
+        dao.find.return_value = returnVal
+        return UserController(dao)
+
+    @pytest.fixture
+    def sut(self):
+        dao = MagicMock()
+        mockres = UserController(dao=dao)
+        return mockres
+
+    # Test case for a valid email
     @pytest.mark.unit
-    def test_get_user_by_email_vallid_email(self, sut, email):
-        vallid, _ = email
-        for mail in vallid:
-            sut.dao.find.return_value = [mail]
-            assert sut.get_user_by_email(mail) == mail
-     
+    def test_validate_email_valid_email(self, sut_one_user):
+        email = 'john.doe@example.com'
+        
+        assert sut_one_user.get_user_by_email(email) == {'firstName': 'John', 'lastName': 'Doe', 'email': 'john.doe@example.com'}
+        
+    # Test case for an multiple user with same email, shuld return only one user
+    @pytest.mark.unit
+    def test_email_multiple(self, sut_multiple_users):
+        email = 'john123@example.com'
+        assert sut_multiple_users.get_user_by_email(email) == {'firstName': 'John', 'lastName': 'Doe', 'email': 'john123@example.com'}
+
     # Test case for an invalid email
-    @pytest.mark.parametrize('invalid_email', [
-        pytest.param(email, id=f'invalid_email_{i}')
-        for i, email in enumerate(email_data[1])
-    ])
-    def test_get_user_by_invalid_email(sut, invalid_email):
-        email = invalid_email[0]
-        # Assert that a ValueError is raised for an invalid email
-        with pytest.raises(ValueError):
-            sut.get_user_by_email(email)
-            
+    invalid_email_1 = 'jane.doeemail.com'
+    invalid_email_2 = '.com'
+    invalid_email_3 = 'this_is_not_an_email'
+    invalid_email_4 = 'jane.doeemail'
+    invalid_email_5 = ''
+
+    @pytest.mark.unit
+    @pytest.mark.parametrize('invalid_email',
+                            [
+                                (invalid_email_1),
+                                (invalid_email_2),
+                                (invalid_email_3),
+                                (invalid_email_4),
+                                (invalid_email_5),
+                            ])
+    def test_get_user_by_email_invalid(self, sut, invalid_email):
+            with pytest.raises(ValueError):
+                sut.get_user_by_email(email = invalid_email)
