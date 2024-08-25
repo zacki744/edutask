58c51806f20c3b66440e54318ab9c5d14d58761e
zacki744 <78082188+zacki744@users.noreply.github.com>
2024-08-25 14:42:24 +0200

    fix cygwin

--- frontend/cypress/e2e/R8UC1.cy.js
+++ frontend/cypress/e2e/R8UC1.cy.js
@@ -2,6 +2,7 @@ describe('Test cases for R8UC1', () => {
   // Variables to store user information
   let uid;
   let email;
+  let task_id;
 
   // Before hook to create a user and login before each test
   before(() => {
@@ -15,65 +16,68 @@ describe('Test cases for R8UC1', () => {
       }).then((response) => {
         uid = response.body._id.$oid;
         email = user.email;
-
-        // Log in the user
-        cy.visit('http://localhost:3000');
-        cy.contains('div', 'Email Address').find('input[type=text]').type(email);
-        cy.get('form').submit();
-        cy.get('#title').type('My New Task');
-        cy.get('#url').type('dQw4w9WgXcQ');
-        cy.get('form').submit();
       });
     });
   });
 
   // Before each hook to login before each test
   beforeEach(() => {
+    cy.fixture("NewTask.json")
+    .then(task => {
+      task.userid = uid,
+      cy.request({
+        method: "POST",
+        url: "http://localhost:5000/tasks/create",
+        form: true,
+        body: task
+      }).then(response => {
+        console.log("TID", response.body)
+        task_id = response.body[0]._id.$oid
+      })
+    })
     cy.visit('http://localhost:3000');
     cy.contains('div', 'Email Address').find('input[type=text]').type(email);
     cy.get('form').submit();
+    cy.get('.title-overlay').last().click();
+    cy.wait(2000);
   });
 
   // Test case 1: should create a new todo item when a non-empty description is entered and the "Add" button is clicked
   it('Test case 1: should create a new todo item when a non-empty description is entered and the "Add" button is clicked', () => {
     const todoDescription = 'Buy groceries';
 
-    // Click on the task to open it
-    cy.get('.title-overlay').last().click();
-    cy.wait(2000);
-
-    // Enter todo description and click "Add"
+    // add a new todo item
     cy.get('.inline-form input[type="text"]').type(todoDescription);
-    cy.get('.inline-form').submit();
-
-    // Check if the new todo item is created
-    cy.get('.todo-list').should('contain', todoDescription);
+    cy.get('.inline-form input[value="Add"]').click();
+    
+    // check if the todo item is added
+    cy.get('.todo-list')
+      .find('.todo-item').last()
+      .find('.editable')
+      .should('have.text', todoDescription);
   });
-
   // Test case 2: should not create a new todo item when an empty description is entered and the "Add" button is clicked
   it('Test case 2: should not create a new todo item when an empty description is entered and the "Add" button is clicked', () => {
     // Click on the task to open it
-    cy.get('.title-overlay').last().click();
-    cy.wait(2000);
-
-    // Ensure the input field is empty
-    cy.get('.inline-form input[type="text"]').should('have.value', '');
-
-    // Click "Add" without entering a description
-    cy.get('.inline-form').submit();
-
-    // Check if no new todo item is created
-    cy.get('.todo-list').should('not.contain', '.todo-item');
+    cy.get('.inline-form input[type="text"]').clear({ force: true });
+    cy.get('.inline-form input[value="Add"]').should('be.disabled');
   });
 
-  // After hook to delete the user after all tests
-  after(() => {
-    // Clean up by deleting the user from the database
+  afterEach(() => {
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/tasks/${task_id}`
+    }).then((response) => {
+      cy.log(response.body)
+    })
+  });
+
+  after(function () {
     cy.request({
       method: 'DELETE',
       url: `http://localhost:5000/users/${uid}`
     }).then((response) => {
-      cy.log(response.body);
-    });
-  });
-});
+      cy.log(response.body)
+    })
+  })
+})
--- frontend/cypress/e2e/R8UC2.cy.js
+++ frontend/cypress/e2e/R8UC2.cy.js
@@ -1,69 +1,97 @@
 describe('Test cases for R8UC2', () => {
-    // Variables to store user information
-    let uid;
-    let email;
-  
-    // Before hook to create a user and login before each test
-    before(() => {
-      cy.fixture('user.json').then((user) => {
-        // Create a user
-        cy.request({
-          method: 'POST',
-          url: 'http://localhost:5000/users/create',
-          form: true,
-          body: user
-        }).then((response) => {
-          uid = response.body._id.$oid;
-          email = user.email;
-  
-          // Log in the user
-          cy.visit('http://localhost:3000');
-          cy.contains('div', 'Email Address').find('input[type=text]').type(email);
-          cy.get('form').submit();
-          cy.get('#title').type('My New Task');
-          cy.get('#url').type('dQw4w9WgXcQ');
-          cy.get('form').submit();
-        });
+  let uid;
+  let email;
+  let task_id;
+  let todo_id;
+
+  // Before hook to create a user and login before each test
+  before(() => {
+    cy.fixture('user.json').then((user) => {
+      // Create a user
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:5000/users/create',
+        form: true,
+        body: user
+      }).then((response) => {
+        uid = response.body._id.$oid;
+        email = user.email;
       });
     });
-  
-    // Before each hook to login before each test
-    beforeEach(() => {
-      cy.visit('http://localhost:3000');
-      cy.contains('div', 'Email Address').find('input[type=text]').type(email);
-      cy.get('form').submit();
-    });
-  
-    // Test case 1: Verify that a todo item can be marked as done when the user clicks on the checkbox icon
-    it('Test case 1: Verify that a todo item can be marked as done when the user clicks on the checkbox icon.', () => {
-      cy.get('.title-overlay').last().click()
-
-      cy.get('.todo-list .todo-item')
-        .eq(0)
-        .find('[class^=checker]')
-        .trigger('click', { force: true }) // Check the checkbox
-        .should('have.class', 'unchecked') // Ensure that the checkbox has the class "done"  
-    })
-
-    it('Test case 2: Verify that a todo item can be marked as active when the user clicks on the checkbox icon agein.', () => {
-      cy.get('.title-overlay').last().click()
-      // Open the first todo item
-      cy.get('.todo-list .todo-item')
-        .eq(0)
-        .find('[class^=checker]')
-        .trigger('click', { force: true }) // Uncheck the checkbox
-        .should('have.class', 'checked') // Ensure that the checkbox has the class "done"s
-    });
-  
-    // After hook to delete the user after all tests
-    after(() => {
-      // Clean up by deleting the user from the database
+    cy.fixture("NewTask.json").then(task => {
+      task.userid = uid;
       cy.request({
-        method: 'DELETE',
-        url: `http://localhost:5000/users/${uid}`
-      }).then((response) => {
-        cy.log(response.body);
+        method: "POST",
+        url: "http://localhost:5000/tasks/create",
+        form: true,
+        body: task
+      }).then(response => {
+        task_id = response.body[0]._id.$oid;
+        todo_id = response.body[0].todos[0]._id.$oid;
       });
     });
   });
-  
+  beforeEach(function() {
+    cy.visit('http://localhost:3000');
+    cy.contains('div', 'Email Address').find('input[type=text]').type(email);
+    cy.get('form').submit();
+    cy.get('.title-overlay').last().click();
+    cy.wait(2000);
+  });
+
+  // Test case 1: Verify that a todo item can be marked as done when the user clicks on the checkbox icon
+  it('Test case 1: Verify that a todo item can be marked as done when the user clicks on the checkbox icon.', () => {
+    const data = `data={'$set': {'done': false}}`;
+    cy.request({
+      method: 'PUT',
+      url: `http://localhost:5000/todos/byid/${todo_id}`,
+      body: data,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    cy.get('.todo-list .todo-item')
+      .eq(0)
+      .find('[class^=checker]')
+      .trigger('click', { force: true }) // Uncheck the checkbox
+      .wait(200);
+
+    cy.get('.todo-list .todo-item')
+      .eq(0)
+      .find('[class^=checker]')
+      .should('have.class', 'checked');
+  });
+
+  // Test case 2: Verify that a todo item can be marked as active when the user clicks on the checkbox icon again
+  it('Test case 2: Verify that a todo item can be marked as active when the user clicks on the checkbox icon again.', () => {
+    const data = `data={'$set': {'done': true}}`;
+    cy.request({
+      method: 'PUT',
+      url: `http://localhost:5000/todos/byid/${todo_id}`,
+      body: data,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    cy.get('.todo-list .todo-item')
+      .eq(0)
+      .find('[class^=checker]')
+      .trigger('click', { force: true })
+      .wait(200);
+
+    cy.get('.todo-list .todo-item')
+      .eq(0)
+      .find('[class^=checker]')
+      .should('have.class', 'unchecked');
+  });
+  after(function () {
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/users/${uid}`
+    }).then((response) => {
+      cy.log(response.body);
+    });
+  });
+});
