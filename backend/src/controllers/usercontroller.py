bebf610daeb7d183d205966f1562f6825544b80d
zacki744 <78082188+zacki744@users.noreply.github.com>
2024-08-25 18:52:15 +0200

    Update usercontroller.py

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -25,21 +25,20 @@ class UserController(Controller):
             Exception -- in case any database operation fails
         """
 
-        users = None
+        if not re.fullmatch(emailValidator, email):
+            raise ValueError('Error: invalid email address')
+
         try:
             users = self.dao.find({'email': email})
-        except Exception as e:
-            raise e
-
-        if users is not None:
             if len(users) == 1:
                 return users[0]
-            elif len(users) > 1:
-                print(f'Error: more than one user found with email {email}')
-                return users[0]
-            else:
+            elif len(users) == 0:
                 return None
-        return users
+            else:
+                print(f'Error: more than one user found with mail {email}')
+                return users[0]
+        except Exception as e:
+            raise
 
     def update(self, id, data):
         try:
